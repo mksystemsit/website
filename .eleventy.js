dbde9c4f1527caa1c86dfa93abe9e37b6cc373b8
MK Systems <rio@mksystems.com.au>
2022-07-23 17:05:09 +1000

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = (config) => {
+  config.addPassthroughCopy({ 'public': './' })
+  config.setBrowserSyncConfig({
+    files: ['dist/**/*'],
+    open: true,
+  })
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+    },
+    markdownTemplateEngine: 'njk',
+  }
+}
